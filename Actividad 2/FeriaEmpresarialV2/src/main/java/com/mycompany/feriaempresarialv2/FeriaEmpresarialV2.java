29b2ef6ccd910d0313c16fb56b66cbb27d6d64c0
LeonardoNietoR <leonardo.nieto.r@gmail.com>
2025-08-12 02:32:16 -0500

    Añadir código Java de Feria Empresarial V2 (Actividad 2)

---
+++ Actividad 2/FeriaEmpresarialV2/src/main/java/com/mycompany/feriaempresarialv2/FeriaEmpresarialV2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.feriaempresarialv2;
+
+/**
+ *
+ * @author userone
+ */
+public class FeriaEmpresarialV2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
